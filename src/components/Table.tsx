4cee9f7b9227564d6336d5d51cbc4e40247d5c82
ashot3232 <asahakyan@sphereinc.com>
2023-09-13 00:01:42 +0400

    Add reuseable and sortable table functionality

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, Fragment, useState } from 'react';
 import styled from 'styled-components';
-import { UserType } from '../types';
+import { GoTriangleDown, GoTriangleUp } from 'react-icons/go';
 
 const TableContainer = styled.div`
   display: flex;
@@ -28,6 +28,19 @@ const TableHeader = styled.th`
   color: white;
 `;
 
+const SortableTableHeader = styled(TableHeader)`
+  cursor: pointer;
+
+  &:hover {
+    background: #04aa6de0;
+  }
+`;
+
+const SortableTableHeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const TableRow = styled.tr`
   &:nth-child(even) {
     background-color: #f2f2f2;
@@ -37,35 +50,151 @@ const TableRow = styled.tr`
   }
 `;
 
-interface TableProps {
-  data: UserType[];
+const IconWrapper = styled.div`
+  display: block;
+  margin-right: 10px;
+`;
+
+interface Column {
+  key: string;
+  label: string;
+  sortValue?(item: any): any;
+  render?(item: any): React.ReactNode;
+  header?(): React.ReactNode;
 }
 
-const Table: FC<TableProps> = ({ data }) => {
+interface TableProps {
+  data: any[];
+  columns: Column[];
+}
+
+const Table: FC<TableProps> = ({ data, columns }) => {
+  const [sortOrder, setSortOrder] = useState<null | string>(null);
+  const [sortBy, setSortBy] = useState<null | string>(null);
+
+  const handleClick = (label: string) => {
+    if (sortBy && label !== sortBy) {
+      setSortOrder('asc');
+      setSortBy(label);
+      return;
+    }
+
+    if (sortOrder === null) {
+      setSortOrder('asc');
+      setSortBy(label);
+    } else if (sortOrder === 'asc') {
+      setSortOrder('desc');
+      setSortBy(label);
+    } else if (sortOrder === 'desc') {
+      setSortOrder(null);
+      setSortBy(null);
+    }
+  };
+
+  const updatedColumns = columns.map((column) => {
+    if (!column.sortValue) {
+      return column;
+    }
+
+    return {
+      ...column,
+      header: () => (
+        <SortableTableHeader
+          key={column.key}
+          onClick={() => handleClick(column.label)}
+        >
+          <SortableTableHeaderContent>
+            {getIcons(column.label, sortBy, sortOrder)}
+            {column.label}
+          </SortableTableHeaderContent>
+        </SortableTableHeader>
+      ),
+    };
+  });
+
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = columns.find(
+      (column) => column.label === sortBy,
+    ) as Column;
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue && sortValue(a);
+      const valueB = sortValue && sortValue(b);
+
+      const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+
+      if (typeof valueA === 'string') {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      }
+
+      return (valueA - valueB) * reverseOrder;
+    });
+  }
+
+  const renderedRows = sortedData.map((item) => {
+    return (
+      <TableRow key={item.id}>
+        {columns.map(({ key, render }) => {
+          if (!render) {
+            return <TableCell key={key}>{item[key as string]}</TableCell>;
+          }
+          return <TableCell key={key}>{render(item)}</TableCell>;
+        })}
+      </TableRow>
+    );
+  });
+
+  const renderedHeaders = updatedColumns.map(({ label, header }) => {
+    if (header) {
+      return <Fragment key={label}>{header()}</Fragment>;
+    }
+
+    return <TableHeader key={label}>{label}</TableHeader>;
+  });
+
   return (
-    <div>
-      <TableContainer>
-        <StyledTable>
-          <TableRow>
-            <TableHeader>Name</TableHeader>
-            <TableHeader>Email</TableHeader>
-            <TableHeader>Age</TableHeader>
-            <TableHeader>Action</TableHeader>
-          </TableRow>
-          {data.map((user) => {
-            return (
-              <TableRow>
-                <TableCell>{user.firstName}</TableCell>
-                <TableCell>{user.email}</TableCell>
-                <TableCell>{user.age}</TableCell>
-                <TableCell>Remove</TableCell>
-              </TableRow>
-            );
-          })}
-        </StyledTable>
-      </TableContainer>
-    </div>
+    <TableContainer>
+      <StyledTable>
+        <thead>
+          <TableRow>{renderedHeaders}</TableRow>
+        </thead>
+        <tbody>{renderedRows}</tbody>
+      </StyledTable>
+    </TableContainer>
   );
 };
+
+function getIcons(label: string, sortBy: any, sortOrder: any) {
+  const upDownIcons = (
+    <div>
+      <IconWrapper>
+        <GoTriangleUp />
+      </IconWrapper>
+      <IconWrapper>
+        <GoTriangleDown />
+      </IconWrapper>
+    </div>
+  );
+
+  if (label !== sortBy) {
+    return upDownIcons;
+  }
+
+  if (sortOrder === null) {
+    return upDownIcons;
+  } else if (sortOrder === 'asc') {
+    return (
+      <IconWrapper>
+        <GoTriangleUp />
+      </IconWrapper>
+    );
+  } else if (sortOrder === 'desc') {
+    return (
+      <IconWrapper>
+        <GoTriangleDown />
+      </IconWrapper>
+    );
+  }
+}
 
 export default Table;
--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -1,10 +1,19 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { AiOutlineDelete } from 'react-icons/ai';
+
 import { fetchUsers, useAppSelector } from '../store';
 import Table from '../components/Table';
-
 import { UserType } from '../types';
 import { useThunk } from '../hooks/useThunk';
+
+const TableContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
 
 function UserList() {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] =
@@ -18,20 +27,52 @@ function UserList() {
     doFetchUsers();
   }, [doFetchUsers]);
 
+  const columns = [
+    {
+      label: 'Name',
+      key: 'name',
+      sortValue: ({ firstName, lastName }: UserType) =>
+        `${firstName} ${lastName}`,
+      render: ({ id, firstName, lastName }: UserType) => {
+        return (
+          <Link to={id}>
+            {firstName} {lastName}
+          </Link>
+        );
+      },
+    },
+    {
+      label: 'Email',
+      key: 'email',
+    },
+    {
+      label: 'Age',
+      key: 'age',
+      sortValue: ({ age }: UserType) => age,
+    },
+    {
+      label: 'Action',
+      key: 'action',
+      render: ({ id }: UserType) => {
+        return (
+          <TableContainer key={id}>
+            <AiOutlineDelete />
+          </TableContainer>
+        );
+      },
+    },
+  ];
+
   let content;
   if (isLoadingUsers) {
     content = <div>Loading...</div>;
   } else if (loadingUsersError) {
     content = <div>Error fetching data...</div>;
   } else {
-    content = <Table data={data} />;
+    content = <Table data={data} columns={columns} />;
   }
 
-  return (
-    <div>
-      <ul>{content}</ul>
-    </div>
-  );
+  return <div>{content}</div>;
 }
 
 export default UserList;
